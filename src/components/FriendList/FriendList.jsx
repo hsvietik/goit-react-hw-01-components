fec20b01b6572b0a91447f1b9b5e0a20efda8a3a
Hanna Svietik <hsvietik@gmail.com>
2023-05-28 20:47:15 +0100

    some changes

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
-import css from './FriendList.module.css';
-export const FriendList = ({}) => {
+// import css from './FriendList.module.css';
+export const FriendList = () => {
   return <ul class="friend-list"></ul>;
 };
 FriendList.propTypes = {
